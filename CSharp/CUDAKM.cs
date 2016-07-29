5d135051d30b052651ec0b27f26c0a1723aaefc9
inlmouse <inlmouse@hotmail.com>
2016-07-29 16:14:51 +0800

    Init

---
+++ CSharp/CUDAKM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace CSharp
+{
+    class CUDAKM
+    {
+        [DllImport("CUDAKMeans.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int kmeans_cuda(bool kmpp, float tolerance, float yinyang_t, UInt32 samples_size,
+            UInt16 features_size, UInt32 clusters_size, UInt32 seed,
+            UInt32 device, UInt32 verbosity, float[] samples,
+             float[] centroids,  UInt32[] assignments);
+    }
+}
---
+++ CSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kmpp = true;
+            float tolerance = 0.1f;
+            float yinyang_t = 0.1f;
+            UInt16 features_size = 2;
+            UInt32 clusters_size = 3;
+            UInt32 samples_size = 300;
+            UInt32 seed = 3;
+            UInt32 device = 0;
+            UInt32 verbosity = 2;
+            float[] samples = new float[samples_size * features_size];
+            float[] centro = new float[features_size*clusters_size];
+            UInt32[] assignments = new uint[samples_size];
+            Random rd = new Random();
+            for (int i = 0; i < samples_size; i++)
+            {
+                samples[i] = Convert.ToSingle(rd.NextDouble());
+                
+            }
+            //samples = new[] {1.1f, 1.2f, 2.1f, 2.2f, 3.0f, 3.3f};
+            int x = CUDAKM.kmeans_cuda(kmpp, tolerance, yinyang_t, samples_size, features_size, clusters_size, seed,
+                device, verbosity, samples,  centro,  assignments);
+            Console.ReadLine();
+        }
+    }
+}
